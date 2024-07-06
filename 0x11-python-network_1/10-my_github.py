869c79fbe0c49d5531db3f70cbc5bad4949b22cd
amedzaphilips <danielamedzaphilips@gmail.com>
2024-07-05 17:20:14 -0700

    alll

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Python script that takes your Github credentials (username and password)
 and uses the Github API to display your id
 """
+
 import requests
 from requests.auth import HTTPBasicAuth
 from sys import argv
